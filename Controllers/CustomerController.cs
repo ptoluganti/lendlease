81cd047293e031c891ee043958f066ae851dc4e3
ptoluganti <ptoluganti>
2016-08-02 15:26:31 +0100

    web api initial

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebLendLease
+{
+    [Route("/api/Customer")]
+    public class CustomerController
+    {
+private static List<Customer> _customers = new List<Customer>(new[] {
+            new Customer() { Id = 1, Name = "Computer" },
+            new Customer() { Id = 2, Name = "Radio" },
+            new Customer() { Id = 3, Name = "Apple" },
+        });
+
+        public IEnumerable<Customer> Get()
+        {
+            return _customers;
+        }
+
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,9 @@
+//WebLendLease
+namespace WebLendLease
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
